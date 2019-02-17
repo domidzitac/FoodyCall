a3613fd3b215ee2fe068c61fdde837522802a9ab
Nik <nikolaos.perakis@tum.de>
2019-02-16 23:52:46 -0800

    new user-field created in database when first sign-in occurs. The id of the field is the ID of the new user

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,6 @@
 var mainApp = {};
 var textBox = document.getElementById("userfield");
+var collection = firebase.firestore().collection('Users');
 
 (function(){
   var firebase = app_firebase;
@@ -11,7 +12,32 @@ var textBox = document.getElementById("userfield");
     uid = user.uid;
     document.getElementById("userfield").innerHTML = "This is my token: ".concat(uid);
     console.log(uid)
-    console.log
+    //console.log(user)
+    //console.log(firebase.auth().AdditionalUserInfo().isNewUser)
+
+    // Create a reference to the cities collection
+    var UsersRef = firebase.firestore().collection("Users");
+
+    var doc = UsersRef.doc(uid);
+    doc.get().then((docData) => {
+    if (docData.exists) {
+      console.log('User already there')
+    } else {
+      // Create new User in the Firecloud database
+      collection.doc(uid).set({
+      UserID: uid,
+      email: user.email,
+      UserName: user.displayName
+      });
+    }
+  }).catch((fail) => {
+    // Either
+    // 1. failed to read due to some reason such as permission denied ( online )
+    // 2. failed because document does not exists on local storage ( offline )
+  });
+
+    
+
     
   }else{
     console.log("2")
