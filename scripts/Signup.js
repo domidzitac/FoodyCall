74169d57e873afc07f12535e36ba97eb771c8fb7
Nik <nikolaos.perakis@tum.de>
2019-02-16 19:07:53 -0800

    Populate username, names, ZIP, ratings

--- scripts/Signup.js
+++ scripts/Signup.js
@@ -1,9 +1,25 @@
 var signupBtn = document.getElementById("signupBtn");
 var gotoSignupBtn = document.getElementById("gotoSignupBtn");
+const form = document.getElementById("add-user");
+
+
 
 function submitClick() {
-  var collection = firebase.firestore().collection('restaurants');
-  return collection.add({a: "a"});
+	console.log(form)
+  var collection = firebase.firestore().collection('Users');
+  console.log(document.getElementById("firstnamesignup").value)
+  collection.add({
+	b: "b",
+    FirstName: form.firstnamesignup.value,
+    LastName: form.lastnamesignup.value,
+    UserName: form.usernamesignup.value,
+	ZIP: Number(form.zipsignup.value),
+	CookRating: 4,
+	EatRating: 3,
+	CookEventsN: 0,	
+	EatEventsN: 0
+});
+  console.log(form.firstnamesignup.value)
 }
 
 
